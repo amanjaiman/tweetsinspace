d0962105008ee6e7437e222d10bfe7cc6fb156bc
Aman Jaiman <amanjaiman@outlook.com>
2019-09-28 18:40:35 -0400

    Generate data with query

--- clean_data.py
+++ clean_data.py
@@ -1,6 +1,6 @@
 import utils
 import pandas as pd
 
-df = utils.get_tweet_info("Apple", 100)
+df = utils.get_tweet_info("Donald Trump", 100)
 
-df.to_csv('apple_geo.csv')
+df.to_csv('data/trump_geo.csv')
